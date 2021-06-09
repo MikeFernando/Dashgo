0a75dd651953fa90545cb3e69ce103aa4b745c0f
Mike Fernando <mikera2020@outlook.com>
2021-06-09 13:56:15 -0300

    Página Sign in

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,8 +12,12 @@ export const theme = extendTheme({
          "300": "#9699B0",
          "200": "#B3B5C6",
          "100": "#D1D2DC",
-         "50": "#EEEEF2",
+         "50": "#EEEEF2"
       }
+   },
+   fonts: {
+      heading: 'Roboto',
+      body: 'Roboto'
    },
    styles: {
       global: {
